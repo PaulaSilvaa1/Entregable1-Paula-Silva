d178ac68c97b9153863901154e2142f4efafcf00
Paula Silva <paulinsilveira12@gmail.com>
2022-09-22 19:49:59 -0500

    Version 2

--- App_Ps/urls.py
+++ App_Ps/urls.py
@@ -7,12 +7,12 @@ from App_Ps.views import *
 urlpatterns = [
    
     path('', inicio, name="Inicio"),
-    path('cursos/', cursos, name="Cursos"),
+    path('cursos/', cursos ),
     path('profesores/', profesores, name="Profesores"),
     path('estudiantes/', estudiantes, name="Estudiantes"),
     path('entregables/', entregables, name="Entregables"),
-    path('formu1/',formulario1),
-    path('formu2/',formulario2),
+    path('formu1/',formulario1,name="Cursos"),
+    path('formu2/',formulario2,name= "Registrarse"),
     path('buscarCursos/',busquedaCursos),
     path('buscar/',buscar),
 ]
